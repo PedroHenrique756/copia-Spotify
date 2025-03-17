bb1986434454f6544b18e265739c872ec773ae17
Pedro Henrique Guedes de Araujo <pedroppft13@gmail.com>
2025-03-17 18:03:54 -0300

    Adição das imagens via JS!

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Imagens dos asrtistas
+    const artistasData = [
+        {nome: 'Alee', image: 'images/artista-alee.jpeg'},
+        {nome: 'Baco Exu', image:'images/artista-baco-exu.jpeg'},
+        {nome: 'Jorge & Mateus', image:'images/jorge-mateus.jpeg'},
+        {nome: 'Matue', image:'images/artista-matue.jpeg'},
+        {nome: 'Veigh', image:'images/artista-veigh.jpeg'},
+        {nome: 'Wiu', image:'images/artista-wiu.jpeg'},
+    ]
+    //imagens dos albuns    
+    const albumData = [
+        {nome: '333', artista: 'Matue',image: 'images/album-333.jpeg'},
+        {nome: '808 CLUB', artista: 'Wiu',image:'images/album-808-club.jpeg'},
+        {nome: 'Dos Predios', artista: 'Veigh',image:'images/album-dos-predios.jpeg'},
+        {nome: 'Escandalo', artista: 'Luíza Sonza',image:'images/album-escandalo.jpeg'},
+        {nome: 'Hit Me', artista: 'Billie Eilish',image:'images/album-hit-me.jpeg'},
+        {nome: 'Tempo.zip', artista: 'Teto',image:'images/album-tempozip-teto.jpeg'},
+    ]
